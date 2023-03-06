010c04ae8cf427c97e9586c5c702530aeb7ca4c3
Daniel Zimmermann <Daniel.Zimmermann@fzi.de>
2023-03-06 17:18:26 +0100

    Modified task

--- run_gui_test_exec.py
+++ run_gui_test_exec.py
@@ -13,8 +13,9 @@ browser.get('https://sea-lion-app-q6nwz.ondigitalocean.app/sample1')
 # Get the HTML source code of the page
 html_source = browser.page_source
 
-task = "Your task is to write a GUI test case using python and selenium for the following web application: " + html_source
+task = "Your task is to test a web application using python and selenium. Start the python code with <StartCode> and finish the code with a <EndCode> label. Only print the code without further explanations. This is the web application: " + html_source
 
+print(task)
 
 completion = openai.ChatCompletion.create(
     model="gpt-3.5-turbo",
