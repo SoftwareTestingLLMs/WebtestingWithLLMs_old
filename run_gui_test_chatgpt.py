eb93f7d0bdb444f0f964d64071c8a4c9f1b834f1
Daniel Zimmermann <Daniel.Zimmermann@fzi.de>
2023-03-06 15:34:46 +0100

    Added loop for multiple interactions

--- run_gui_test_chatgpt.py
+++ run_gui_test_chatgpt.py
@@ -2,6 +2,7 @@ from selenium import webdriver
 from selenium.webdriver.common.by import By
 import openai
 import json
+import time
 
 # Get openai key from file
 with open("openai_key.json", "r") as file:
@@ -11,31 +12,34 @@ with open("openai_key.json", "r") as file:
 browser = webdriver.Chrome()
 browser.get('https://sea-lion-app-q6nwz.ondigitalocean.app/sample1')
 
-# Get the HTML source code of the page
-html_source = browser.page_source
+while True:
+    # Get the HTML source code of the page
+    html_source = browser.page_source
 
-task = "Your task is to interact with random a gui button element of the web application below using Selenium's WebDriver API. First, give me the corresponding XPath expression of the gui element you want to interact with and then give me corresponding action, for example \"click\". Put the XPath expression and the action in a json dict with the following keys: \"xpath\" and \"action\". This is the html source of the web application: " + html_source
+    task = "Your task is to interact with a random gui button element of the web application below using Selenium's WebDriver API. First, give me the corresponding XPath expression of the gui element you want to interact with and then give me corresponding action, for example \"click\". Put the XPath expression and the action in a json dict with the following keys: \"element\" and \"action\". This is the html source of the web application: " + html_source
 
-completion = openai.ChatCompletion.create(
-    model="gpt-3.5-turbo",
-    messages=[{"role": "user", "content": task}]
-)
+    completion = openai.ChatCompletion.create(
+        model="gpt-3.5-turbo",
+        messages=[{"role": "user", "content": task}]
+    )
 
-response = completion["choices"][0]["message"]["content"]
-response_dict = json.loads(response)
-print(response_dict)
+    response = completion["choices"][0]["message"]["content"]
+    response_dict = json.loads(response)
+    print(response_dict)
 
-# Perform the action on the web element using Selenium
-element = browser.find_element(By.XPATH, response_dict['xpath'])
-action = response_dict['action']
-if action == 'click':
-    element.click()
-elif action == 'submit':
-    element.submit()
-elif action == 'clear':
-    element.clear()
-else:
-    raise ValueError(f"Unknown action {action}")
+    # Perform the action on the web element using Selenium
+    element = browser.find_element(By.XPATH, response_dict['element'])
+    action = response_dict['action']
+    if action == 'click':
+        element.click()
+    elif action == 'submit':
+        element.submit()
+    elif action == 'clear':
+        element.clear()
+    else:
+        raise ValueError(f"Unknown action {action}")
+    
+    time.sleep(5)
 
 # Close the web browser
 browser.quit()
