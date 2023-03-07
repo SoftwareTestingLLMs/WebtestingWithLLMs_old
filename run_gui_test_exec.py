d85c3ab4d21e02789b1506a11acd8b779ae37555
Daniel Zimmermann <Daniel.Zimmermann@fzi.de>
2023-03-07 17:20:35 +0100

    Using click library

--- run_gui_test_exec.py
+++ run_gui_test_exec.py
@@ -1,42 +1,52 @@
+import click
 from selenium import webdriver
 import openai
 import json
 
-url = 'https://sea-lion-app-q6nwz.ondigitalocean.app/sample1'
+# Define the command-line options
 
-# Get openai key from file
-with open("openai_key.json", "r") as file:
-    openai.api_key = json.load(file)["key"]
 
-# Open the web browser and navigate to the app's URL
-browser = webdriver.Chrome()
-browser.get(url)
+@click.command()
+@click.option('--url', default='https://sea-lion-app-q6nwz.ondigitalocean.app/sample1', help='The URL of the web application to test.')
+def main(url):
 
-# Get the HTML source code of the page
-html_source = browser.page_source
+    # Get openai key from file
+    with open("openai_key.json", "r") as file:
+        openai.api_key = json.load(file)["key"]
 
-# Close the web browser
-browser.quit()
+    # Open the web browser and navigate to the app's URL
+    browser = webdriver.Chrome()
+    browser.get(url)
 
-task = "Your task is to test a web application using python and selenium with the URL " + url + " . Start the python code with <StartCode> and finish the code with a <EndCode> label. Use \"browser = webdriver.Chrome()\" to open the web browser. Use only xpath commands like \"browser.find_element(By.XPATH, '//button[text()=\"Click me!\"]')\" to find elements. If there is an alert, the script should switch to the alert and dismiss it before proceeding with the next step. Use assertions to test the correct behavior of the application. Only print the code without further explanations. This is the web application: " + html_source
+    # Get the HTML source code of the page
+    html_source = browser.page_source
 
-print(task)
+    # Close the web browser
+    browser.quit()
 
-completion = openai.ChatCompletion.create(
-    model="gpt-3.5-turbo",
-    messages=[{"role": "user", "content": task}]
-)
+    task = f"Your task is to test a web application using python and selenium with the URL {url}. Start the python code with <StartCode> and finish the code with a <EndCode> label. Use \"browser = webdriver.Chrome()\" to open the web browser. Use only xpath commands like \"browser.find_element(By.XPATH, '//button[text()=\"Click me!\"]')\" to find elements. If there is an alert, the script should switch to the alert and dismiss it before proceeding with the next step. Use assertions to test the correct behavior of the application. Only print the code without further explanations. This is the web application: {html_source}"
 
-response = completion["choices"][0]["message"]["content"]
-print(response)
+    click.echo(task)
 
-# Extract code between <StartCode> and <EndCode> labels
-start_index = response.find("<StartCode>") + len("<StartCode>")
-end_index = response.find("<EndCode>")
-code_string = response[start_index:end_index]
+    completion = openai.ChatCompletion.create(
+        model="gpt-3.5-turbo",
+        messages=[{"role": "user", "content": task}]
+    )
 
-# Execute the code
-exec(code_string)
+    response = completion["choices"][0]["message"]["content"]
+    click.echo(response)
 
-# Close the web browser
-browser.quit()
+    # Extract code between <StartCode> and <EndCode> labels
+    start_index = response.find("<StartCode>") + len("<StartCode>")
+    end_index = response.find("<EndCode>")
+    code_string = response[start_index:end_index]
+
+    # Execute the code
+    exec(code_string)
+
+    # Close the web browser
+    browser.quit()
+
+
+if __name__ == '__main__':
+    main()
