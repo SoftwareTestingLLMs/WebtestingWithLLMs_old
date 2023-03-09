0deca720481f82c348879baf8684d4963e33bd75
Daniel Zimmermann <Daniel.Zimmermann@fzi.de>
2023-03-09 09:55:26 +0100

    Using pythons unittest module

--- static_gui_tests.py/run_gui_test_static.py
+++ static_gui_tests.py/run_gui_test_static.py
@@ -1,39 +1,68 @@
+import unittest
 from selenium import webdriver
 from selenium.webdriver.common.by import By
+from selenium.webdriver.support.ui import WebDriverWait
+from selenium.webdriver.support import expected_conditions as EC
 from bs4 import BeautifulSoup
 
-# Open the web browser and navigate to the app's URL
-browser = webdriver.Chrome()
-browser.get('https://sea-lion-app-q6nwz.ondigitalocean.app/sample1')
+class TestWebApp(unittest.TestCase):
 
-# Get the HTML source code of the page
-html_source = browser.page_source
+    def setUp(self):
+        # Open the web browser and navigate to the app's URL
+        self.browser = webdriver.Chrome()
+        self.browser.get('https://sea-lion-app-q6nwz.ondigitalocean.app/sample1')
 
-# Use BeautifulSoup to parse the HTML source code
-soup = BeautifulSoup(html_source, 'html.parser')
+    def tearDown(self):
+        # Close the web browser
+        self.browser.quit()
 
-# Find the input field and enter text into it
-input_field = browser.find_element(By.ID, 'inputField')
-input_field.send_keys('Hello, world!')
+    def test_input_field(self):
+        # Find the input field and enter text into it
+        input_field = self.browser.find_element(By.ID, 'inputField')
+        input_field.send_keys('Hello, world!')
 
-# Find the "Click me!" button and click it
-click_button = browser.find_element(By.XPATH, '//button[text()="Click me!"]')
-click_button.click()
+        # Find the "Click me!" button and click it
+        click_button = self.browser.find_element(By.XPATH, '//button[string()="Click me!"]')
+        click_button.click()
 
-# Check if the alert message is displayed with the correct text
-alert = browser.switch_to.alert
-assert 'You typed: Hello, world!' == alert.text
-alert.accept()
+        # Wait for the alert to appear and get its text
+        alert = WebDriverWait(self.browser, 10).until(EC.alert_is_present())
+        alert_text = alert.text
 
-# Find the "Clear" button and click it
-clear_button = browser.find_element(By.XPATH, '//button[text()="Clear"]')
-clear_button.click()
+        # Check if the alert message is displayed with the correct text
+        self.assertEqual('You typed: Hello, world!', alert_text)
 
-# Check if the input field is cleared
-assert '' == input_field.get_attribute('value')
+        # Dismiss the alert
+        alert.accept()
 
-# Print the HTML structure of the page
-print(soup.prettify())
+        # Find the "Clear" button and click it
+        clear_button = self.browser.find_element(By.XPATH, '//button[string()="Clear"]')
+        clear_button.click()
 
-# Close the web browser
-browser.quit()
+        # Check if the input field is cleared
+        self.assertEqual('', input_field.get_attribute('value'))
+
+    def test_html_structure(self):
+        # Get the HTML source code of the page
+        html_source = self.browser.page_source
+
+        # Use BeautifulSoup to parse the HTML source code
+        soup = BeautifulSoup(html_source, 'html.parser')
+
+        # Check if the page title is correct
+        self.assertEqual('My JavaScript Application', soup.title.string)
+
+        # Check if the page contains an input field
+        input_field = soup.find('input', {'id': 'inputField'})
+        self.assertIsNotNone(input_field)
+
+        # Check if the page contains a "Click me!" button
+        click_button = soup.find('button', string='Click me!')
+        self.assertIsNotNone(click_button)
+
+        # Check if the page contains a "Clear" button
+        clear_button = soup.find('button', string='Clear')
+        self.assertIsNotNone(clear_button)
+
+if __name__ == '__main__':
+    unittest.main()
