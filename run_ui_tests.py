a8b6ec89d0e7a9bd24ee9f115078dbf443eb6ef6
Daniel Zimmermann <Daniel.Zimmermann@fzi.de>
2023-03-09 10:32:07 +0100

    Moved tests to subdirectory

--- run_ui_tests.py
+++ run_ui_tests.py
@@ -1,6 +1,19 @@
+import click
 import unittest
 
-loader = unittest.TestLoader()
-tests = loader.discover('ui_tests')
-testRunner = unittest.runner.TextTestRunner()
-testRunner.run(tests)
+
+@click.command()
+@click.option('--path', default='ui_tests/sea-lion-app-q6nwz.ondigitalocean.app/sample1',
+              help='Path to the directory containing the test files.')
+def run_tests(path):
+    """
+    Runs all test cases in the specified directory.
+    """
+    loader = unittest.TestLoader()
+    tests = loader.discover(path)
+    testRunner = unittest.runner.TextTestRunner()
+    testRunner.run(tests)
+
+
+if __name__ == '__main__':
+    run_tests()
