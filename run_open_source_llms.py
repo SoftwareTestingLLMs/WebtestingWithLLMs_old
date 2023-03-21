357d8d14688a83b097b1a726c733f60a6c2a46ce
Patrick Deubel <33164686+pdeubel@users.noreply.github.com>
2023-03-21 15:09:17 +0100

    Added saving of generated scripts

--- run_open_source_llms.py
+++ run_open_source_llms.py
@@ -1,11 +1,15 @@
+import os
+from datetime import datetime
 from multiprocessing import Process
 
 from tools.hf_model_utils import load_model, generate, truncate
 
+GENERATED_SCRIPTS_PATH = "generated_scripts"
 
-def run_model(histories, prompt_list, model_name):
+
+def run_model(histories, prompt_list, model_name, folder_path):
     print("Loading model...")
-    tokenizer, model = load_model(model_name, "cuda:0", "float32")
+    tokenizer, model = load_model(model_name, "cpu", "float32")
     print("Loading finished")
 
     def _wrap(p):
@@ -13,8 +17,12 @@ def run_model(histories, prompt_list, model_name):
 
     for i, prompt in enumerate(prompt_list):
         histories = [h + _wrap(prompt) for h in histories]
-        completions = generate(histories, tokenizer, model, "cuda:0")
+        completions = generate(histories, tokenizer, model, "cpu")
         histories = [h + f"{truncate(c, model_name)}\n\n" for h, c in zip(histories, completions)]
+
+        # Prettify: removes two of the four newlines
+        if histories[0][-4:] == "\n\n\n\n":
+            histories[0] = histories[0][:-2]
 
         print("-" * 10)
         print(i)
@@ -22,8 +30,16 @@ def run_model(histories, prompt_list, model_name):
         print(histories[0])
         print("-" * 10)
 
+    with open(os.path.join(folder_path, f"{model_name.replace('/', '-')}.py"), "w") as f:
+        f.write(histories[0])
+
 
 def main():
+    # Setup folder
+    start_data = datetime.now().strftime("%Y-%m-%d_%H-%M-%S")
+    folder_path = os.path.join(GENERATED_SCRIPTS_PATH, start_data)
+    os.makedirs(folder_path, exist_ok=True)
+
     models = ["Salesforce/codegen-350M-mono", "Salesforce/codegen-2B-mono"]
 
     history = ["# Import libraries.\n\nimport numpy as np\n\n"]
@@ -33,9 +49,10 @@ def main():
                "Sort these characters in alphabetical order.", "Print the resulting list of characters."]
 
     for model_name in models:
-        input(f"Start run with model {model_name}")
         p = Process(target=run_model, args=(history, prompts, model_name))
         p.start()
+        p.join()
+        p.close()
 
 
 if __name__ == "__main__":
